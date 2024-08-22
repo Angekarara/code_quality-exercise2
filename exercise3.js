6b83e1a6c979b304f4c613b19546a2303e1a60fc
Angekarara <mbabange2020@gmail.com>
2024-08-22 10:55:23 +0200

    Improved codes ex3

--- exercise3.js
+++ exercise3.js
@@ -1,7 +1,3 @@
 function reverseString(str) {
-  var reversed = "";
-  for (var i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
+  return str.split("").reverse().join("");
 }
