0fa9ed7f73aa988a8525c8015666ec890c57e006
Angekarara <mbabange2020@gmail.com>
2024-08-22 10:56:13 +0200

    First codes ex4

---
+++ exercise4.js
@@ -0,0 +1,9 @@
+function findEvenNumbers(numbers) {
+  var evenNumbers = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      evenNumbers.push(numbers[i]);
+    }
+  }
+  return evenNumbers;
+}
