79b6bf2f9ad940f75bbb361f01e4435b1dde7cec
Angekarara <mbabange2020@gmail.com>
2024-08-22 10:51:46 +0200

    Improved codes ex1

--- exercise1.js
+++ exercise1.js
@@ -1,10 +1,3 @@
 function calculateAverage(numbers) {
-    var sum = 0;
-    var count = 0;
-    for (var i = 0; i < numbers.length; i++) {
-      sum += numbers[i];
-      count++;
-    }
-    var average = sum / count;
-    return average;
-  }
+  return numbers.reduce((acc, num) => acc + num) / numbers.length;
+}
