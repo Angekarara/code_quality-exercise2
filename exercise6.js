2bbdc804c9f66e040ca6a02c6edebafebc0f0852
Angekarara <mbabange2020@gmail.com>
2024-08-22 11:01:07 +0200

    Improved codes ex6

--- exercise6.js
+++ exercise6.js
@@ -1,11 +1,3 @@
 function maxNumber(a, b, c) {
-  if (a > b && a > c) {
-    return a;
-  } else {
-    if (b > a && b > c) {
-      return b;
-    } else {
-      return c;
-    }
-  }
+  return Math.max(a, b, c);
 }
