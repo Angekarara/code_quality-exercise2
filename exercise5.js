49dea5d2f48a3f4d13aab47fded693c6fc797776
Angekarara <mbabange2020@gmail.com>
2024-08-22 11:21:20 +0200

    More improvement on ex5

--- exercise5.js
+++ exercise5.js
@@ -1,4 +1,2 @@
-function calculateFactorial(n) {
-  if (n === 0 || n === 1) return 1;
-  return n * calculateFactorial(n - 1);
-}
+const calculateFactorial = (n) =>
+  n === 0 || n === 1 ? 1 : n * calculateFactorial(n - 1);
