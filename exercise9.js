beface7132ded528e4c3bff670e7ea69c5eb70ca
Angekarara <mbabange2020@gmail.com>
2024-08-22 11:07:07 +0200

    Improved cods ex9

--- exercise9.js
+++ exercise9.js
@@ -1,6 +1,6 @@
 function calculateMatrixSum(matrix) {
-  var sum = 0;
-  for (var i = 0; i < matrix.length; i++)
-    for (var j = 0; j < matrix[i].length; j++) sum += matrix[i][j];
-  return sum;
+  return matrix.reduce(
+    (acc, row) => acc + row.reduce((sum, num) => sum + num, 0),
+    0
+  );
 }
